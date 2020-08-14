66aa66c909b3b58b5a65fde05a9066bc95715d3a
Unathi Skosana <ukskosana@gmail.com>
2020-08-14 17:51:40 +0200

    init

---
+++ utils.py
@@ -0,0 +1,17 @@
+flatten = lambda l: np.array([item for sl in l for item in sl])
+vectorize_im = lambda im,m: flatten(im.reshape((m,1)))
+
+def bmatrix(a):
+    """Returns a LaTeX bmatrix
+    :a: numpy array
+    :returns: LaTeX bmatrix as a string
+    """
+    if len(a.shape) > 2:
+        raise ValueError('bmatrix can at most display two dimensions')
+    lines = str(a).replace('[', '').replace(']', '').splitlines()
+    rv = [r'\begin{bmatrix}']
+    rv += ['  ' + ' & '.join(l.split()) + r'\\' for l in lines]
+    rv +=  [r'\end{bmatrix}']
+    return '\n'.join(rv)
+
+
