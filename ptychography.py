c72a02efc76feabbd6bbfbf2e95139ce32828e08
Unathi Skosana <ukskosana@gmail.com>
2020-08-15 14:54:07 +0200

    Add abberations

--- ptychography.py
+++ ptychography.py
@@ -10,6 +10,9 @@ from skimage import io,transform,color,img_as_float
 from numpy import pi
 from scipy.fft import fft2, ifft2, fft, ifft, fftshift, ifftshift
 from scipy.signal import convolve
+from utils import coherent_low_pass_filter, \
+    incoherent_low_pass_filter, wavefront_abber, \
+    coherent_low_pass_filter_abber, incoherent_low_pass_filter_abber
 
 # TeX typesetting
 from matplotlib import rc
@@ -24,44 +27,38 @@ if __name__ == "__main__":
     im_path = sys.argv[1]
     im = img_as_float(io.imread(im_path, as_gray=True))
     im_amp =  np.sqrt(im)
+    im_ft = fftshift(fft2(im_amp))
+    h,w = im_ft.shape
 
     # parameter
     wave_length =  0.5e-6
     k = 2 * pi / wave_length
     pixel_size = 0.5e-6
     NA = 0.1
-    cutoff = NA * k
+    cutoff_freq = NA * k
 
-    im_ft = fftshift(fft2(im_amp))
-    m,n = im_ft.shape
-    kx = np.arange(-pi/pixel_size, pi/pixel_size, 2*pi / pixel_size / n)
-    ky = np.arange(-pi/pixel_size, pi/pixel_size, 2*pi / pixel_size / m)
-    kxv, kyv = np.meshgrid(kx, ky)
+    #coher_ft, coher_ctf = coherent_low_pass_filter(im_ft, pixel_size, \
+    #        cutoff_freq)
+    #incoher_ft, incoher_otf = incoherent_low_pass_filter(im_ft, pixel_size, \
+    #        cutoff_freq)
 
-    freq = kxv**2 + kyv**2
-    sft = np.copy(freq)
-    ctf = freq <= cutoff**2
-    ctf = ctf.astype(int)
-
-    cpsf = fftshift(ifft2(ifftshift(ctf)))
-    ipsf = np.abs(cpsf)**2
-    otf  = np.abs(fftshift(fft2(ifftshift(ipsf))))
-
-    out_ft = np.multiply(im_ft, ctf)
-    out_amp = np.abs(ifft2(ifftshift(out_ft)))
-    out_int = out_amp**2
-
-    out_ft_otf = np.multiply(im_ft, otf)
-    out_amp_otf = np.abs(ifft2(ifftshift(out_ft_otf)))
+    phi = wavefront_abber([(2,2,20)], w, h)
+    coher_aber_ft, coher_aber_ctf = coherent_low_pass_filter_abber(phi, im_ft,
+            pixel_size,
+            cutoff_freq)
 
     fig = plt.figure(figsize=(4., 4.))
     grid = ImageGrid(fig, 111,  # similar to subplot(111)
-                 nrows_ncols=(2,3),  # creates 2x2 grid of axes
+                 nrows_ncols=(1,3),  # creates 2x2 grid of axes
                  axes_pad=0.1,  # pad between axes in inch.
                 )
 
     for ax, img in zip(grid,
-            [im_amp,out_amp,out_amp_otf,np.log(np.abs(im_ft)),np.log(np.abs(out_ft)),otf]):
+                [
+                im_amp,
+                np.abs(ifft2(ifftshift(coher_aber_ft))),
+                phi
+                ]):
         # Iterating over the grid returns the Axes.
         ax.imshow(img, cmap='gray')
     plt.show()
--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+import numpy as np
+from numpy import pi
+from scipy.fft import fft2, ifft2, fft, ifft, fftshift, ifftshift
+
+# --- formatting --- #
 flatten = lambda l: np.array([item for sl in l for item in sl])
 vectorize_im = lambda im,m: flatten(im.reshape((m,1)))
 
@@ -14,4 +19,67 @@ def bmatrix(a):
     rv +=  [r'\end{bmatrix}']
     return '\n'.join(rv)
 
+# --- abberations --- #
+from zernike import RZern
+from operator import itemgetter
 
+def wavefront_abber(coefficients, width, height):
+    nn = max(coefficients, key=itemgetter(1))[1]
+    cart = RZern(nn)
+    ddx = np.linspace(-1.0, 1.0, width)
+    ddy = np.linspace(-1.0, 1.0, height)
+    xv, yv = np.meshgrid(ddx, ddy)
+    cart.make_cart_grid(xv, yv)
+    c = np.zeros(cart.nk)
+
+    for coeff in coefficients:
+        m,n,v = coeff
+        j =  int(n * (n + 1) / 2 + np.abs(m))
+        if m >= 0 and n % 4 == 2 or n % 4 == 3:
+            j += 1
+        if m <= 0 and n % 4 == 0 or n % 4 == 1:
+            j += 1
+        c[j-1] = v
+    phi = cart.eval_grid(c, matrix=True)
+    return np.nan_to_num(phi, nan=1.0)
+
+
+# --- filters --- #
+def coherent_low_pass_filter(im_ft,pixel_size,cutoff_freq):
+    m,n = im_ft.shape
+    kx = np.arange(-pi/pixel_size, pi/pixel_size, 2*pi / pixel_size / n)
+    ky = np.arange(-pi/pixel_size, pi/pixel_size, 2*pi / pixel_size / m)
+    kxv, kyv = np.meshgrid(kx, ky)
+
+    freq = kxv**2 + kyv**2
+    ctf = freq <= cutoff_freq**2
+    out_ft = np.multiply(im_ft, ctf)
+
+    return out_ft,ctf
+
+def incoherent_low_pass_filter(im_ft,pixel_size,cutoff_freq):
+    _,ctf = coherent_low_pass_filter(im_ft, pixel_size, cutoff_freq)
+    cpsf = fftshift(ifft2(ifftshift(ctf)))
+    ipsf = np.abs(cpsf)**2
+    otf  = np.abs(fftshift(fft2(ifftshift(ipsf))))
+    otf = otf/np.max(otf)
+    out_ft = np.multiply(im_ft, otf)
+
+    return out_ft,otf
+
+
+def coherent_low_pass_filter_abber(phi,im_ft,pixel_size,cutoff_freq):
+    _, ctf = coherent_low_pass_filter(im_ft,pixel_size,cutoff_freq)
+    aber_ctf = np.multiply(np.exp(1j * phi), ctf)
+    aber_ft = np.multiply(im_ft, aber_ctf)
+
+    return aber_ft, aber_ctf
+
+def incoherent_low_pass_filter_abber(phi, im_ft,pixel_size,cutoff_freq):
+    _,aber_ctf = coherent_low_pass_filter_abber(phi, im_ft, pixel_size, cutoff_freq)
+    cpsf = fftshift(ifft2(ifftshift(aber_ctf)))
+    ipsf = np.abs(cpsf)**2
+    otf  = np.abs(fftshift(fft2(ifftshift(ipsf))))
+    out_ft = np.multiply(im_ft, otf)
+
+    return out_ft,otf
