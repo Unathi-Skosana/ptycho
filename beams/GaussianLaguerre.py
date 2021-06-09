Fix beam scaling bug

beams/GaussianLaguerre.py
@@ -7,7 +7,6 @@ Description:
     Adapted from https://github.com/PaulKGrimes/GaussianBeams/blob/master/GaussianLaguerreModes.py
 """
 
-
 from math import factorial
 from numpy import pi,  sqrt, arctan, exp
 from scipy.special import eval_genlaguerre
@@ -27,6 +26,9 @@ def beam_curv(z, z0):
     Returns:
         radius of curvature
     """
+    if z == 0:
+        return np.Inf
+
     return z *(1 + (z0 / z)**2)
 
 
@@ -43,7 +45,7 @@ def beam_rad(z, w0, z0):
         radius at which the amplitude fall to 1/e
     """
  
-    return w0 * sqrt(1 + (z/z0)**2)
+    return w0 * sqrt(1 + (z / z0)**2)
 
 
 def rayleigh_range(w0, k):
@@ -88,8 +90,22 @@ def alpha(r, w):
         dimensionless parameter
     """
 
-    return np.sqrt(2) * r / w
+    return sqrt(2) * r / w
 
+def scale(r, z, z0):
+    """
+    Computes dimensionless parameter involving the radial distance and the beam
+    curvature for calculation convenience in the near field. (F'' in  Tuovinen)
+
+    Args:
+        r: radial distance from the center axis of the beam
+        z: axial distance from beam's waist
+        z0: rayleigh range
+    """
+
+    R = beam_curv(z, z0)
+
+    return sqrt(1 + (r / R)**2)
 
 def lag_pl(x, p=0, l=0, normalize=True):
     """
@@ -128,12 +144,11 @@ def amplitude(r, z, k, w0, p=0, l=0):
     z0 = rayleigh_range(w0, k)
     w = beam_rad(z, w0, z0)
     a = alpha(r, w)
-    R = beam_curv(z, z0)
-    FF = np.sqrt(1 + (r / R)**2)
-    a = np.sqrt(2) * r / (w * FF)
-    cosTh = 1.0/FF
+    FF = scale(r, z, z0)
+    a /= FF
 
-    return (1 + cosTh) / 2 * 1/(k * w *FF) * a**abs(l) * lag_pl(a**2, p, abs(l))
+    return np.array((1.0 + 1.0/FF) / 2.0 * 1/(k * w *FF) * a**abs(l) *
+            lag_pl(a**2, p, abs(l)), dtype=np.float128)
 
 
 def phase(r, phi, z, k, w0, p=0, l=0):
@@ -157,14 +172,14 @@ def phase(r, phi, z, k, w0, p=0, l=0):
     w = beam_rad(z, w0, z0)
     R = beam_curv(z, z0)
     a = alpha(r, w)
-    FF = np.sqrt(1 + (r / R)**2)
-    a = np.sqrt(2) * r / (w * FF)
+    FF = scale(r, z, z0)
+    a /= FF
 
-    return exp(- (a**2) / 2 
+    return np.array(exp(-a**2 / 2.0
                - 1j * k * R * (FF - 1)
                - 1j * k * z
                + 1j * (2 * p + abs(l) + 1) * phi0(z, z0)
-               + 1j * l * phi)
+               - 1j * l * phi), dtype=np.complex128)
 
 
 def gauss_lag_modes(r, phi, z, k, w0, p=0, l=0):

beams/GaussianLaguerreBeams.py
@@ -91,8 +91,8 @@ class GaussLaguerreModeSet(GaussLaguerreModeBase):
 
         for p in range(coeffs.shape[0]):
             for l in range(coeffs.shape[1]):
-                result += coeffs[p, l] * GLM(r, phi, z, self.w0,
-                                             self.k, p=p, l=l)
+                result += coeffs[p, l] * GLM(r, phi, z, self.k,
+                                             self.w0, p=p, l=l)
         return result
 
 
@@ -129,8 +129,8 @@ class ModifiedGaussLaguerreModeSet(GaussLaguerreModeBase):
         result = np.zeros_like(r, dtype=np.complex64)
         for p in range(coeffs.shape[0]):
             for l in range(coeffs.shape[1]):
-                result += coeffs[p, l] * modGLM_nf(r, phi, z, self.w0,
-                                                   self.k, p=p, l=l)
+                result += coeffs[p, l] * modGLM_nf(r, phi, z, self.k,
+                                                   self.w0, p=p, l=l)
         return result
 
     def far_field(self, theta, phi, z, coeffs):
@@ -157,6 +157,6 @@ class ModifiedGaussLaguerreModeSet(GaussLaguerreModeBase):
         result = np.zeros_like(theta, dtype=np.complex64)
         for p in range(coeffs.shape[0]):
             for l in range(coeffs.shape[1]):
-                result += coeffs[p, l] * modGLM_ff(theta, phi, z, self.w0,
-                                                   self.k, p=p, l=l)
+                result += coeffs[p, l] * modGLM_ff(theta, phi, z, self.k,
+                                                   self.w0, p=p, l=l)
         return result

tests/test_engine.py
@@ -37,61 +37,70 @@ if __name__ == "__main__":
     obj = amp * np.exp(j * phase)
     height, width = obj.shape
 
-    ptycho = PytchoSimulator(alpha=0.05, beta=1.0, probe=70,
+    ptycho = PytchoSimulator(alpha=0.25, beta=1.0, probe=70,
                              start=(20, 20), shift=5, rc=(30, 30),
-                             iterations=30)
-    width = 70
-    zz = 100
-    xx, yy = np.meshgrid(np.mgrid[-35:35:width*j], np.mgrid[-35:35:width*j])
+                             iterations=50)
+    wavelength = 624*1e-9
+    k = 2 * np.pi / wavelength
+    grid_size = 30 / wavelength
+    w0 = 100 * wavelength
+    N = 70
+    dx = grid_size / N
 
-    # Calculate the cylindrical coordinates
-    rr = np.sqrt(xx**2 + yy**2)
-    phi = np.arctan2(yy, xx)
+    w = N
+    cx = int(w/2)
 
-    GL_beam = GLM(w0=0.35, k=20, maxL=6, maxP=6)
-    GH_beam = GHM(w0=70, k=20, maxL=6, maxM=6)
+    h = N
+    cy = int(h/2)
 
-    GL_c = np.zeros(GL_beam.shape)
-    GL_c[1, 2] = 1.0
+    Y, X = np.mgrid[:h, :w]
+    Y = (Y-cy) * dx
+    X = (X-cx) * dx
 
-    GH_c = np.zeros(GH_beam.shape)
-    GH_c[1, 3] = 1.0
+    zz = 1*1e-9
 
-    GH_illu_func = GH_beam.field(xx, yy, zz, GH_c)
-    GL_illu_func = GL_beam.field(rr, phi, zz, GL_c)
+    beam = GLM(w0 = w0, k = k, maxP=8, maxL=8)
+    rr = np.sqrt(X**2 + Y**2)
+    phi = np.arctan2(Y, X)
 
-    cmask = circ_mask(70, (70//2,70//2), 35, 1.0, inverse=False)
+    c = np.zeros(beam.shape)
+    c[2, 2] = 1.0
+    u1 = beam.field(rr, phi, zz, c)
 
-    GL_illu_func = (np.abs(GL_illu_func) * cmask) * np.exp(1j *
-            (rescale_intensity(np.angle(GL_illu_func), out_range=(0, 1.0)) * cmask))
+    cmask = circ_mask(70, (70//2, 70//2), 35, 1.0, inverse=False)
+    u1 = (np.abs(u1) * cmask) * np.exp(1j *
+            (rescale_intensity(np.angle(u1) * cmask, out_range=(0, 1.0))))
 
-    phi = zernike_modes([(0, 6, 1.0)], ptycho.probe, width)
-    illu_func = cmask * np.exp(j * np.abs(phi))
+    c = np.zeros(beam.shape)
+    c[0, 0] = 1.0
+    u2 = beam.field(rr, phi, zz, c)
+    u2 = np.abs(u2)
 
-    diff_patterns = ptycho.diffract(obj, GL_illu_func)
+    diff_patterns = ptycho.diffract(obj, u1, mode="poisson", mean=500)
     err_ival = 1
+
     obj_est, illu_func_est, RMS, R_factor = ptycho.repie(obj, diff_patterns,
             err_ival=err_ival, hold=5, permute=True)
 
     fig0, ax0 = plt.subplots()
-    ax0.imshow(rescale_intensity(np.abs(GL_illu_func), out_range=(0.0, 1.0)),
+    ax0.imshow(rescale_intensity(np.abs(obj_est), out_range=(0.0, 1.0)),
             cmap='gray', vmin=0.0, vmax=1.0)
     ax0.set_axis_off()
 
     fig1, ax1 = plt.subplots()
-    cax1 = ax1.imshow(rescale_intensity(np.angle(obj_est), out_range=(-np.pi, np.pi)),
+    cax1 = ax1.imshow(rescale_intensity(unwrap_phase(np.angle(obj_est)), out_range=(-np.pi, np.pi)),
             cmap='gray', vmin=-np.pi, vmax=np.pi)
-    cbar = fig1.colorbar(cax1, ticks=[-np.pi, 0, np.pi], shrink=0.7)
-    cbar.ax.set_yticklabels([r'$-\pi$', r'$0$', r'$\pi$'])
     ax1.set_axis_off()
 
     fig2, ax2 = plt.subplots()
     x = range(0, ptycho.iterations, err_ival)
     ax2.plot(x, RMS, 'C1o:', mec="1.0")
+    ax2.set_title("RMS")
 
     fig3, ax3 = plt.subplots()
     x = range(0, ptycho.iterations, err_ival)
     ax3.plot(x, R_factor, 'C2o:', mec="1.0")
+    ax3.set_title("R-factor")
 
     fig4, ax4 = plt.subplots()
     ax4.imshow(rescale_intensity(np.abs(illu_func_est), out_range=(0.0, 1.0)),
@@ -101,40 +110,18 @@ if __name__ == "__main__":
     fig5, ax5 = plt.subplots()
     cax5 = ax5.imshow(rescale_intensity(np.angle(illu_func_est), out_range=(-np.pi, np.pi)),
             cmap='gray', vmin=-np.pi, vmax=np.pi)
-    cbar = fig5.colorbar(cax5, ticks=[-np.pi, 0, np.pi], shrink=0.7)
-    cbar.ax.set_yticklabels([r'$-\pi$', r'$0$', r'$\pi$'])
+
     ax5.set_axis_off()
 
-    # fig6, ax6 = plt.subplots()
-    # ax6.imshow(rescale_intensity(np.abs(diff_patterns[11]), out_range=(0.0, 1.0)), cmap='RdBu', vmin=0.0, vmax=1.0)
-    # ax6.set_axis_off()
+    fig6, ax6 = plt.subplots()
+    ax6.imshow(rescale_intensity(np.abs(u1), out_range=(0.0, 1.0)),
+            cmap='gray', vmin=0.0, vmax=1.0)
+    ax6.set_axis_off()
 
-    # fig7, ax7 = plt.subplots()
-    # ax7.imshow(rescale_intensity(np.abs(diff_patterns[12]), out_range=(0.0, 1.0)), cmap='RdBu', vmin=0.0, vmax=1.0)
-    # ax7.set_axis_off()
+    fig7, ax7 = plt.subplots()
+    cax7 = ax7.imshow(rescale_intensity(unwrap_phase(np.angle(u1)), out_range=(-np.pi, np.pi)),
+            cmap='gray', vmin=-np.pi, vmax=np.pi)
+    ax7.set_axis_off()
 
-    # fig8, ax8 = plt.subplots()
-    # ax8.imshow(rescale_intensity(np.abs(diff_patterns[13]), out_range=(0.0, 1.0)), cmap='RdBu', vmin=0.0, vmax=1.0)
-    # ax8.set_axis_off()
 
-    fig0.savefig('epie_obj_amplitude.png', bbox_inches='tight',
-                 pad_inches=0, transparent=False)
-    fig1.savefig('epie_obj_phase.png', bbox_inches='tight',
-                 pad_inches=0, transparent=False)
-    fig4.savefig('epie_probe_amplitude.png', bbox_inches='tight',
-                 pad_inches=0, transparent=False)
-    fig5.savefig('epie_probe_phase.png', bbox_inches='tight',
-                 pad_inches=0, transparent=False)
-    # fig4.savefig('rms.png', bbox_inches='tight',
-    #              pad_inches=0, transparent=False)
-    # fig5.savefig('diff1.png', bbox_inches='tight',
-    #              pad_inches=0, transparent=False)
-    # fig6.savefig('diff2.png', bbox_inches='tight',
-    #              pad_inches=0, transparent=False)
-    # fig7.savefig('diff3.png', bbox_inches='tight',
-    #              pad_inches=0, transparent=False)
-    # fig8.savefig('diff4.png', bbox_inches='tight',
-    #              pad_inches=0, transparent=False)
-    # fig9.savefig('R_factor.png', bbox_inches='tight',
-    #             pad_inches=0, transparent=False)
     plt.show()

tests/test_gh.py
@@ -15,70 +15,42 @@ if __name__ == '__main__':
 
     plt.style.use('mint')
 
-    beam = GHM(w0=80, k=5.0, maxL=6, maxM=6)
-    zz = 5.0
-    width = 80
-    xx, yy = np.meshgrid(np.mgrid[-20:20:width*j], np.mgrid[-20:20:width*j])
+    grid_size = 1*10e6
+    wavelength = 624*10e-9
+    w0 = 3*10e-6
+    N = 256
+    dx = grid_size / N
+
+    w = N
+    cx = int(w/2)
+    xvals = dx * np.arange(-cx, (w-cx))
+
+    h = N
+    cy = int(h/2)
+    yvals = dx * np.arange(-cy, (h-cy))
+
+    Y, X = np.mgrid[:h, :w]
+    Y = (Y-cy) * dx
+    X = (X-cx) * dx
+
+    z = 1*10e-6
+
+    beam = GHM(w0=w0, k=2 * np.pi / wavelength, maxL=6, maxM=6)
 
     c = np.zeros(beam.shape)
-    c[1, 2] = 1.0
-    u = beam.field(xx, yy, zz, c)
+    c[0, 0] = 1.0
+
+    u = beam.field(X, Y, z, c)
 
     fig1, axes1 = plt.subplots(1, 2)
     ax1, ax2 = axes1.ravel()
+
     ax1.set_title(r'Amplitude')
     ax1.set_axis_off()
     ax1.imshow(np.abs(u), cmap='RdBu')
+
     ax2.set_title('Phase')
     ax2.set_axis_off()
     ax2.imshow(np.angle(u), cmap='RdBu')
-
-    c = np.zeros(beam.shape)
-    c[2, 3] = 1.0
-    u = beam.field(xx, yy, zz, c)
-
-    fig2, axes2 = plt.subplots(1, 2)
-    ax3, ax4 = axes2.ravel()
-    ax3.set_title(r'Amplitude')
-    ax3.set_axis_off()
-    ax3.imshow(np.abs(u), cmap='RdBu')
-    ax4.set_title('Phase')
-    ax4.set_axis_off()
-    ax4.imshow(np.angle(u), cmap='RdBu')
-
-    c = np.zeros(beam.shape)
-    c[3, 4] = 1.0
-    u = beam.field(xx, yy, zz, c)
-
-    fig3, axes3 = plt.subplots(1, 2)
-    ax5, ax6 = axes3.ravel()
-    ax5.set_title(r'Amplitude')
-    ax5.set_axis_off()
-    ax5.imshow(np.abs(u), cmap='RdBu')
-    ax6.set_title('Phase')
-    ax6.set_axis_off()
-    ax6.imshow(np.angle(u), cmap='RdBu')
-
-    c = np.zeros(beam.shape)
-    c[4, 5] = 1.0
-    u = beam.field(xx, yy, zz, c)
-
-    fig4, axes4 = plt.subplots(1, 2)
-    ax7, ax8 = axes4.ravel()
-    ax7.set_title(r'Amplitude')
-    ax7.set_axis_off()
-    ax7.imshow(np.abs(u), cmap='RdBu')
-    ax8.set_title('Phase')
-    ax8.set_axis_off()
-    ax8.imshow(np.angle(u), cmap='RdBu')
-
-    fig1.savefig('gh12.png', bbox_inches='tight',
-                 pad_inches=0, transparent=False)
-    fig2.savefig('gh23.png', bbox_inches='tight',
-                 pad_inches=0, transparent=False)
-    fig3.savefig('gh34.png', bbox_inches='tight',
-                 pad_inches=0, transparent=False)
-    fig4.savefig('gh45.png', bbox_inches='tight',
-                 pad_inches=0, transparent=False)
 
     plt.show()

tests/test_gl.py
@@ -10,49 +10,87 @@ if __name__ == '__main__':
     import numpy as np
     import matplotlib.pyplot as plt
     from skimage.restoration import unwrap_phase
+    from skimage.exposure import rescale_intensity
+    from mpl_toolkits.axes_grid1 import ImageGrid
 
     from common.constants import j
     from beams.GaussianLaguerreBeams import GaussLaguerreModeSet as GLM
+    from utils.filters import circ_mask
 
     plt.style.use('mint')
 
-    grid_size = 10e6
-    wavelength = 624*10e-9
-    w0 = 3*10e-6
+    wavelength = 624 * 1e-9
+    k = 2 * np.pi / wavelength
+    w0 = 3 * 1e-6
     N = 256
-    dx = grid_size / N
 
-    w = N
-    cx = int(w/2)
-    xvals = dx * np.arange(-cx, (w-cx))
+    L = 20 * 1e-6
+    x0 = np.linspace(-L/2, L/2, N)
+    y0 = np.linspace(-L/2, L/2, N)
+    xv, yv = np.meshgrid(x0, y0)
+    zz = 1*1e-9
 
-    h = N
-    cy = int(h/2)
-    yvals = dx * np.arange(-cy, (h-cy))
+    beam = GLM(w0 = w0, k = k, maxP=8, maxL=8)
+    rr = np.sqrt(xv**2 + yv**2)
+    phi = np.arctan2(yv, xv)
 
-    Y, X = np.mgrid[:h, :w]
-    Y = (Y-cy) * dx
-    X = (X-cx) * dx
-
-    zz = 1*10e-6
-
-    beam = GLM(w0=w0, k = 2 * np.pi / wavelength, maxP=6, maxL=6)
-    rr = np.sqrt(X**2 + Y**2)
-    phi = np.arctan2(Y, X)
+    cmask = circ_mask(70, (70//2, 70//2), 35, 1.0, inverse=False)
 
     c = np.zeros(beam.shape)
-    c[2, 8]= 1
-    u = beam.field(rr, phi, zz, c)
+    c[0, 0] = 1.0
+    u1 = beam.field(rr, phi, zz, c)
+    c[0, 0] = 0.0
+    c[1, 0] = 1.0
+    u2 = beam.field(rr, phi, zz, c)
+    c[1, 0] = 0.0
+    c[2, 0] = 1.0
+    u3 = beam.field(rr, phi, zz, c)
+    c[2, 0] = 0.0
+    c[0, 1] = 1.0
+    u4 = beam.field(rr, phi, zz, c)
+    c[0, 1] = 0.0
+    c[1, 1] = 1.0
+    u5 = beam.field(rr, phi, zz, c)
+    c[1, 1] = 0.0
+    c[2, 1] = 1.0
+    u6 = beam.field(rr, phi, zz, c)
+    c[2, 1] = 0.0
+    c[0, 2] = 1.0
+    u7 = beam.field(rr, phi, zz, c)
+    c[0, 2] = 0.0
+    c[1, 2] = 1.0
+    u8 = beam.field(rr, phi, zz, c)
+    c[1, 2] = 0.0
+    c[2, 2] = 1.0
+    u9 = beam.field(rr, phi, zz, c)
 
-    fig1, axes1 = plt.subplots(1, 2)
-    ax1, ax2 = axes1.ravel()
+    rr[rr < 1*1e-6] = 0
+    print(rr)
 
-    ax1.set_title(r'Amplitude')
-    ax1.set_axis_off()
-    ax1.imshow(np.abs(u), cmap='RdBu')
+    fig = plt.figure(figsize=(25, 25))
+    grid = ImageGrid(fig,
+            111,  # similar to subplot(111)
+            nrows_ncols=(3, 6),  # creates 2x2 grid of axes
+            axes_pad=0.05,  # pad between axes in inch.
+    )
 
-    ax2.set_title('Phase')
-    ax2.set_axis_off()
-    ax2.imshow(unwrap_phase(np.angle(u)), cmap='RdBu')
+    i = 0
+    for ax, im in zip(grid, [np.abs(u1), np.angle(u1), np.abs(u2), np.angle(u2),
+        np.abs(u3), np.angle(u3), np.abs(u4), np.angle(u4), np.abs(u5),
+        np.angle(u5), np.abs(u6), np.angle(u6), np.abs(u7), np.angle(u7),
+        np.abs(u8), np.angle(u8), np.abs(u9), np.angle(u9)]):
 
+        if i % 2 == 0 and i < 6:
+            ax.set_title(r"$p = {}$".format(i//2), size=40, loc='right', x=1.20)
+
+        if i % 3 == 0:
+            ax.set_ylabel(r"$l = {}$".format(i//6), size=40)
+
+        ax.set_yticks([])
+        ax.set_xticks([])
+
+        ax.imshow(im, cmap="RdBu")
+        i += 1
+    fig.savefig('GLM.png', bbox_inches='tight',
+                  pad_inches=0, transparent=False)
     plt.show()

tests/test_modgl.py
@@ -16,78 +16,47 @@ if __name__ == '__main__':
 
     plt.style.use('mint')
 
-    mod_beam = modGLM(w0=10e-3, k=10e9, maxP=6, maxL=6)
-    width = 70
-    zz_nf = 0.1
-    zz_ff = 50
-    xx, yy = np.meshgrid(np.mgrid[-10:10:width*j], np.mgrid[-10:10:width*j]);
-    rr = np.sqrt(xx**2 + yy**2);
-    theta = np.arcsin(rr / zz_ff)
-    phi = np.arctan2(yy, xx);
+    wavelength = 624*1e-9
+    k = 2 * np.pi / wavelength
+    grid_size = 30/ wavelength
+    w0 = 100 * wavelength
+    N = 70
+    dx = grid_size / N
+
+    w = N
+    cx = int(w/2)
+
+    h = N
+    cy = int(h/2)
+
+    Y, X = np.mgrid[:h, :w]
+    Y = (Y-cy) * dx
+    X = (X-cx) * dx
+
+    z_nf = 1*1e-9
+    z_ff = 5*1e9
+
+    mod_beam = modGLM(w0=w0, k=k, maxP=6, maxL=6)
+
+    r = np.sqrt(X**2 + Y**2);
+    theta = np.arcsin(r / z_ff)
+    phi = np.arctan2(Y, X);
 
     c = np.zeros(mod_beam.shape)
-    c[1, 2] = 0.5
-    u_nf = mod_beam.near_field(rr, phi, zz_nf, c)
-    u_ff = mod_beam.far_field(theta, phi, zz_ff, c)
+    c[2, 2] = 1.0
+
+    u_nf = mod_beam.near_field(r, phi, z_nf, c)
+    u_ff = mod_beam.far_field(theta, phi, z_ff, c)
 
     fig1, axes1 = plt.subplots(1, 2)
     ax1, ax2 = axes1.ravel()
+
     ax1.set_title(r'Amplitude')
     ax1.set_axis_off()
     ax1.imshow(np.abs(u_ff), cmap='RdBu')
+
     ax2.set_title('Phase')
     ax2.set_axis_off()
     ax2.imshow(np.angle(u_ff), cmap='RdBu')
-
-    c = np.zeros(mod_beam.shape)
-    c[2, 3] = 1.0
-    u_ff = mod_beam.near_field(rr, phi, zz_ff, c)
-    u_ff = mod_beam.far_field(theta, phi, zz_ff, c)
-
-    fig2, axes2 = plt.subplots(1, 2)
-    ax3, ax4 = axes2.ravel()
-    ax3.set_title(r'Amplitude')
-    ax3.set_axis_off()
-    ax3.imshow(np.abs(u_ff), cmap='RdBu')
-    ax4.set_title('Phase')
-    ax4.set_axis_off()
-    ax4.imshow(np.angle(u_ff), cmap='RdBu')
-
-    c = np.zeros(mod_beam.shape)
-    c[3, 4] = 1.0
-    u_ff = mod_beam.near_field(rr, phi, zz_ff, c)
-    u_ff = mod_beam.far_field(theta, phi, zz_ff, c)
-
-    fig3, axes3 = plt.subplots(1, 2)
-    ax5, ax6 = axes3.ravel()
-    ax5.set_title(r'Amplitude')
-    ax5.set_axis_off()
-    ax5.imshow(np.abs(u_ff), cmap='RdBu')
-    ax6.set_title('Phase')
-    ax6.set_axis_off()
-    ax6.imshow(np.angle(u_ff), cmap='RdBu')
-
-    c = np.zeros(mod_beam.shape)
-    c[4, 5] = 1.0
-    u_ff = mod_beam.near_field(rr, phi, zz_ff, c)
-    u_ff = mod_beam.far_field(theta, phi, zz_ff, c)
-
-    fig4, axes4 = plt.subplots(1, 2)
-    ax7, ax8 = axes4.ravel()
-    ax7.set_title(r'Amplitude')
-    ax7.set_axis_off()
-    ax7.imshow(np.abs(u_ff), cmap='RdBu')
-    ax8.set_title('Phase')
-    ax8.set_axis_off()
-    ax8.imshow(np.angle(u_ff), cmap='RdBu')
-
-    fig1.savefig('ff_gl12.png', bbox_inches='tight',
-                 pad_inches=0, transparent=False)
-    fig2.savefig('ff_gl23.png', bbox_inches='tight',
-                 pad_inches=0, transparent=False)
-    fig3.savefig('ff_gl34.png', bbox_inches='tight',
-                 pad_inches=0, transparent=False)
-    fig4.savefig('ff_gl45.png', bbox_inches='tight',
-                 pad_inches=0, transparent=False)
 
     plt.show()